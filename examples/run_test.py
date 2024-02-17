585c2ae38dc896762358902b175d7c7a9d5a5a9d
sachuriga sachuriga <sachuriga3@gmail.com>
2024-02-17 17:01:07 +0100

    1.12

--- examples/run_test.py
+++ examples/run_test.py
@@ -1,16 +1,24 @@
 import sys
 sys.path.append('Q:\sachuriga\Sachuriga_Python\quality_metrix')
 
+from preprocess.copy_file import copy_file
 from preprocess.load_data import load_data
 from postprocess.quality_metrix import qualitymetrix
 
-#getting sorted files
-sorted_files = load_data(r'S:\Sachuriga\Ephys_Recording\CR_CA1\65410', file_suffix='_phy_k')
+def main():
+    #getting sorted files
+    folder_path = r'S:/Sachuriga/Ephys_Recording/CR_CA1/65410'
+    sorted_files = load_data(folder_path, file_suffix='_phy_k')
 
-#for file in sorted_files:
-#quality_metrix and export to new phy folder
+    #for file in sorted_files:
+    #quality_metrix and export to new phy folder
 
-for file in sorted_files:
-    qualitymetrix(file)
+    for file in sorted_files[9:]:
+        print(file)
+        qualitymetrix(file)
+        #copy_file(file, file +"_cured")
 
-print("completet!!!!")
+    print("completet!!!!")
+
+if __name__== "__main__":
+    main()
--- postprocess/quality_metrix.py
+++ postprocess/quality_metrix.py
@@ -6,6 +6,7 @@ from spikeinterface.preprocessing import (bandpass_filter, notch_filter, common_
                                           correct_motion, load_motion_info)
 import spikeinterface.exporters as sex
 import spikeinterface.qualitymetrics as sqm
+from pathlib import Path
 
 def main() -> object:
     """
@@ -15,12 +16,7 @@ def main() -> object:
     print("main")
 def qualitymetrix(path):
 
-    sorting = se.read_phy(folder_path=path)
-    sampling_frequency = 30_000.0  # Adjust according to your MATLAB dataset
-    num_channels = 64  # Adjust according to your MATLAB dataset
-    dtype_int = 'int16'  # Adjust according to your MATLAB dataset
-    gain_to_uV = 0.195  # Adjust according to your MATLAB dataset
-    offset_to_uV = 0  # Adjust according to your MATLAB dataset
+    sorting = se.read_phy(folder_path=path, load_all_cluster_properties=True,exclude_cluster_groups = ["noise", "mua"])
     global_job_kwargs = dict(n_jobs=24, chunk_size=10000, chunk_duration="1s", total_memory="32G")
     si.set_global_job_kwargs(**global_job_kwargs)
     temp_path = path.split("_phy")
@@ -36,13 +32,6 @@ def qualitymetrix(path):
             stream_name = 'Record Node 101#Acquisition_Board-100.Rhythm Data'
             recording = se.read_openephys(raw_path, stream_name=stream_name, load_sync_timestamps=True)
 
-    #recording = si.read_binary(file_paths=path + '/recording.bin', sampling_frequency=sampling_frequency,
-    #                           num_channels=num_channels, dtype=dtype_int,
-    #                           gain_to_uV=gain_to_uV, offset_to_uV=offset_to_uV)
-    
-    rec = bandpass_filter(recording, freq_min=300, freq_max=6000)
-    rec_save = common_reference(rec, reference='global', operator='median')
-    
     import probeinterface as pi
 
     # from probeinterface import plotting
@@ -62,36 +51,42 @@ def qualitymetrix(path):
     ]
 
     probe.set_device_channel_indices(mapping_to_device)
+    probe.to_dataframe(complete=True).loc[:, ["contact_ids", "shank_ids", "device_channel_indices"]]
     probegroup = pi.ProbeGroup()
     probegroup.add_probe(probe)
 
     pi.write_prb(f"{probe_name}.prb", probegroup, group_mode="by_shank")
-    recording_prb = rec_save.set_probe(probe, group_mode="by_shank")
+    recording_prb = recording.set_probe(probe, group_mode="by_shank")
+    rec = bandpass_filter(recording_prb, freq_min=300, freq_max=6000)
+    rec_save = common_reference(rec, reference='global', operator='median')
 
-    wf = si.extract_waveforms(recording_prb, sorting, folder='D:/temp_waveform/', overwrite=True, allow_unfiltered=True)
+    sorting.set_property(key='group', values=sorting.get_property("channel_group"))
+
+    wf = si.extract_waveforms(rec_save, sorting, folder='C:/temp_waveform/', overwrite=True, 
+                              allow_unfiltered=True, 
+                              sparse=True, method="by_property",by_property="group")
+
 
     spike_locations = post.compute_unit_locations(waveform_extractor=wf,
                                                   method='center_of_mass',
                                                   radius_um=50.)
 
-    from spikeinterface.postprocessing import compute_principal_components
+    from spikeinterface.postprocessing import compute_principal_components,compute_template_metrics
     pca = compute_principal_components(waveform_extractor=wf, n_components=5, mode="by_channel_local")
     metrics = sqm.compute_quality_metrics(waveform_extractor=wf)
     assert 'isolation_distance' in metrics.columns
-    path_iron = path + "_cured"
+    compute_template_metrics(wf)
+    path_iron = Path(path + "_manual")
     sex.export_to_phy(waveform_extractor=wf,
                       output_folder = path_iron,
                       remove_if_exists=True,
-                      use_relative_path=True,
                       copy_binary=True)
 
-    wf.save(path + "_cured/waveforms", format='binary')
+    wf.save(Path(path + "_manual/waveforms"), format='binary')
     recp = bandpass_filter(recording_prb, freq_min=1, freq_max=475)
     reclaimer = common_reference(recp, reference='global', operator='median')
     si.write_binary_recording(reclaimer, path_iron / 'recording_lfp.bin', dtype='int16')
     si.write_binary_recording(rec_save, path_iron / 'recording_hf.bin', dtype='int16')
-    si.write_binary_recording(recording, path_iron / 'recording_raw.bin', dtype='int16')
-
 
 if __name__ == "__main__":
     main()
